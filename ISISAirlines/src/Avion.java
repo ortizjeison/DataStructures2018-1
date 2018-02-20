08fbc3424145363ea0b4f687f998a8a85f2cdbee
ortizjeison <jeisonfernelix@gmail.com>
2018-02-20 18:43:24 -0500

    atributos y constructores añadidos

--- ISISAirlines/src/Avion.java
+++ ISISAirlines/src/Avion.java
@@ -1,4 +1,7 @@
 
 public class Avion {
 
+	protected String matricula;
+	protected int horasVuelo;
+	//protected enum estado;
 }
---
+++ ISISAirlines/src/Vuelo.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class Vuelo {
+	protected Avion [] aviones;
+	protected String[] idTiquetes;
+	protected Empleado[] tripulacion;
+	protected String IDvuelo; 
+	protected Date fechaSalida;
+	protected Date fechaRealSaida;
+	protected Date fechaLlegada;
+	protected double duracionVuelo;
+	protected Aeropuerto origen;
+	protected Aeropuerto destino;
+}
